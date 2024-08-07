bcab62885aee1611fc44c4e2ec2d65091e1f993b
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-08-07 17:58:04 -0400

    Propiedad 'impuesto' de Cliente, con getter y setter. Método calcularImpuesto().

--- cliente.js
+++ cliente.js
@@ -1,13 +1,29 @@
-export class Cliente{
-    constructor(nombre){
-        this._nombre = nombre;
-    }
+//import { Impuesto } from "./impuestos.js";
 
+export class Cliente{
+    constructor(nombre, impuesto = {}){
+    //constructor(nombre, impuesto = new Impuesto()){
+        this._nombre = nombre;
+        this._impuesto = impuesto;
+    }
+    /** */
     get nombre(){
         return this._nombre;
     }
 
     set nombre(nombre){
         this._nombre = nombre;
+    }
+    /** */
+    get impuesto(){
+        return this._impuesto;
+    }
+
+    set impuesto(impuesto){
+        this._impuesto = impuesto;
+    }
+
+    calcularImpuesto(){
+        return (this._impuesto.montoBrutoAnual - this._impuesto.deducciones) * 0.21 || 0;
     }
 }
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import { Cliente } from "./cliente.js";
 import { Impuesto } from "./impuestos.js";
 
+console.log("--------------------------------PROBANDO--------------------------------");
+
 let c1 = new Cliente("Mario");
 let i1 = new Impuesto();
 
@@ -10,3 +12,14 @@ console.log(i1);
 console.log(c1.nombre);
 c1.nombre = "Super Mario";
 console.log(c1.nombre);
+
+let c2 = new Cliente();
+console.log(c2);
+console.log(c2.nombre);
+
+console.log(c1.calcularImpuesto());
+c1.impuesto = new Impuesto(200, 10);
+console.log(c1);
+console.log(c1.nombre);
+console.log(c1.impuesto);
+console.log(c1.calcularImpuesto());
